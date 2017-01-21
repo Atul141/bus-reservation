dd0ff28baaa73884ffdb88f39a17babc438ff7fa
Atul Kumar <atulbk@thoughtworks.com>
2017-01-21 23:39:42 +0530

    Added migrations using flyway

---
+++ src/main/resources/db/migration/V2017_01_21_23_22_40_73__create_account_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE userDetails
+(
+id int PRIMARY KEY ,
+firstName VARCHAR NOT NULL ,
+lastName VARCHAR(100) NOT NULL ,
+email VARCHAR(100) NOT NULL ,
+phone VARCHAR(100) NOT NULL ,
+password VARCHAR (100) NOT NULL
+
+);
