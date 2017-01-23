d678829f4258e952f3b9fdf7fde3ca417fc0d516
Atul Kumar <atulbk@thoughtworks.com>
2017-01-23 16:19:33 +0530

    Implemeneted qeureing users based on email

--- src/main/java/ServiceImpl/LoginImpl.java
+++ src/main/java/ServiceImpl/LoginImpl.java
@@ -1,19 +1,11 @@
 package ServiceImpl;
 
 
-import Config.HibernateUtil;
 import Dao.LoginDao;
-import Dao.UserDetailsDao;
-import Models.UserDetails;
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class LoginImpl {
@@ -21,7 +13,9 @@ public class LoginImpl {
         public boolean validateLogin(LoginDao loginDao){
             Session session = new Configuration().configure().buildSessionFactory().openSession();
             Transaction transaction = session.beginTransaction();
-            List employees = session.createQuery("FROM UserDetailsDao  user where user.id=6004").list();
+            String qeury="FROM UserDetailsDao  user where user.email="+"'"+loginDao.getEmail()+"'";
+            List employees;
+            employees = session.createQuery(qeury).list();
             transaction.commit();
             session.close();
             if(!employees.isEmpty())
