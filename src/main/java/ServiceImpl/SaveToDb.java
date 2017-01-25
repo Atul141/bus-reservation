faf2186aaba108c01cf304e6bf35b95f2c6d5d5a
Atul Kumar <atulbk@thoughtworks.com>
2017-01-25 12:48:08 +0530

    Restructured ServiecImpl

---
+++ src/main/java/ServiceImpl/SaveToDb.java
@@ -0,0 +1,28 @@
+package ServiceImpl;
+
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class SaveToDb {
+
+
+    public void saveToDb(Object object) {
+
+
+        try {
+
+            Session session = new Configuration().configure().buildSessionFactory().openSession();
+
+            Transaction transaction = session.beginTransaction();
+            session.persist(object);
+            session.flush();
+            transaction.commit();
+            session.close();
+        } catch (Throwable ex) {
+            System.out.println("error creating session " + ex);
+
+        }
+    }
+}
--- src/main/java/ServiceImpl/UserDetailsImpl.java
+++ src/main/java/ServiceImpl/UserDetailsImpl.java
@@ -1,29 +1,17 @@
 package ServiceImpl;
 
 import Dao.UserDetailsDao;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 
 public class UserDetailsImpl {
 
-
+    private SaveToDb saveToDb;
+    public UserDetailsImpl(){
+        saveToDb =new SaveToDb();
+    }
     public void saveUserDetails(UserDetailsDao userDetails) {
+        saveToDb.saveToDb(userDetails);
 
 
-        try {
-
-            Session session = new Configuration().configure().buildSessionFactory().openSession();
-
-            Transaction transaction = session.beginTransaction();
-            session.persist(userDetails);
-            session.flush();
-            transaction.commit();
-            session.close();
-        } catch (Throwable ex) {
-            System.out.println("error creating session "+ex);
-
-        }
     }
 
 }
