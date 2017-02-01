a56cba4cb388a8d886b064cd3b50927d9fdeada2
Atul Kumar <atulbk@thoughtworks.com>
2017-02-01 15:03:36 +0530

    Changed Config Db

--- src/main/java/ServiceImpl/ConfigDB.java
+++ src/main/java/ServiceImpl/ConfigDB.java
@@ -1,13 +1,53 @@
 package ServiceImpl;
 
 
+import Dao.*;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import static ServiceImpl.SyntaxSugar.PROD_ENV;
 
 public class ConfigDB {
 
+    private String environment;
+
+    public ConfigDB() {
+        environment = PROD_ENV;
+    }
+
     public Session getSession() {
-        Session session = new Configuration().configure().buildSessionFactory().openSession();
+
+        if (environment == PROD_ENV) {
+
+            Session session = new Configuration().configure().buildSessionFactory().openSession();
+            return session;
+        }
+        Session session = getTestSession();
+        return session;
+
+    }
+
+    public void setEnvironment(String env) {
+        environment = env;
+    }
+
+    private Session getTestSession() {
+        SessionFactory sessionFactory;
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(UserDetailsDao.class);
+        configuration.addAnnotatedClass(RouteDao.class);
+        configuration.addAnnotatedClass(SeatsDao.class);
+        configuration.addAnnotatedClass(BusDao.class);
+        configuration.addAnnotatedClass(PassengerDao.class);
+        configuration.setProperty("hibernate.dialect",
+                "org.hibernate.dialect.PostgreSQL94Dialect");
+        configuration.setProperty("hibernate.connection.driver_class",
+                "org.postgresql.Driver");
+        configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/testdb");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "create");
+        sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
         return session;
     }
 }
---
+++ src/main/java/ServiceImpl/SyntaxSugar.java
@@ -0,0 +1,10 @@
+package ServiceImpl;
+
+
+public class SyntaxSugar {
+
+    public static final String PROD_ENV = "production";
+    public static final String TEST_ENV = "test";
+
+
+}
